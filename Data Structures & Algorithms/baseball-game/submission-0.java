c0d14e958f021715a816c0234a08a955e0cf636d
Michelle Ung <131570501+mung569@users.noreply.github.com>
2026-04-23 14:06:20 -0400

    Marked optimal solution

--- Data Structures & Algorithms/baseball-game/submission-0.java
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -1,12 +1,11 @@
 class Solution {
+    // *Optimal solution*
     public int calPoints(String[] operations) {
         int[] record = new int[operations.length];
         int top = -1;
         int totalSum = 0;
 
         for (int i = 0; i < operations.length; i++) {
-            // consider converting to switch/case:
-            
             if (operations[i].equals("+")) {
                 record[top+1] = record[top] + record[top - 1];
                 top++;
