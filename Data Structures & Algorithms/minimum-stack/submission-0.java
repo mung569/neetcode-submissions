10cfd9289c5661f884b8fce60179ad26dc443be6
Michelle Ung <131570501+mung569@users.noreply.github.com>
2026-04-23 19:43:55 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,54 @@
+class MinStack {
+    int top, capacity;
+    int[] stack;
+
+    public MinStack() {
+        top = -1;
+        stack = new int[1];
+        capacity = 1;
+    }
+    
+    public void push(int val) {
+        if (top == capacity - 1) { // if stack is full
+            this.resize();
+        }
+        stack[top + 1] = val;
+        top++;
+    }
+    
+    public void pop() {
+        if (top >= 0) top--;
+    }
+    
+    public int top() {
+        if (top < 0) {
+            throw new EmptyStackException();
+        }
+        return stack[top];
+    }
+    
+    public int getMin() {
+        int min = stack[0];
+
+        for (int i=0; i <= top; i++) {
+            if (stack[i] < min) {
+                min = stack[i];
+            }
+        } // for
+
+        return min;
+    }
+
+    // Added functions:
+    public void resize() {
+        capacity = capacity * 2;
+        int[] resizedArray = new int[capacity];
+
+        // copy values
+        for (int i=0; i<stack.length; i++) {
+            resizedArray[i] = stack[i];
+        } // for
+
+        stack = resizedArray;
+    }
+}
