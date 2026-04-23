bef4f0e479af243538de489c0974e05b1c4990b4
Michelle Ung <131570501+mung569@users.noreply.github.com>
2026-04-22 23:17:17 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int length = nums.length * 2;
+        int[] ans = new int[length];
+
+        for (int i = 0; i < ans.length; i++) {
+            if (i < nums.length) {
+                ans[i] = nums[i];
+            }
+            else ans[i] = nums[i - nums.length];
+        }
+
+        return ans;
+    }
+}
